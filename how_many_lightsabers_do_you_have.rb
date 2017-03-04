467f646436bd2b6d0af378de8aac6bfda58b1a61
Michelle Gienow <michellegienow@gmail.com>
2017-03-03 21:27:35 -0500

    The only person who owns lightsabers is Zach by the way

---
+++ how_many_lightsabers_do_you_have.rb
@@ -0,0 +1,16 @@
+#Description:
+
+#Inspired by the development team at Vooza, write the function howManyLightsabersDoYouOwn that
+#accepts the name of a programmer, and
+#returns the number of lightsabers owned by that person.
+#The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+#No starting help here -- you'll need to know how to write a function that accepts a parameter and returns a value based on that parameter.
+
+def howManyLightsabersDoYouOwn(name=0)
+  if name == "Zach"
+    return 18
+  else
+    return 0
+  end
+end
